7b85feea9bf8e76f1df841454cccac18fd0332ed
Rubens Batista <batistarubinho@gmail.com>
2020-03-25 18:50:24 -0300

    Criada a estrutura HTML da pagina Register

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,7 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiArrowLeft } from "react-icons/fi";
+
+import logoImage from "../../assets/logo.svg";
 
 const Register = () => {
-  return <h1>Register</h1>;
+  return (
+    <div className="register-container">
+      <div className="content">
+        <section className="section-info">
+          <img src={logoImage} alt="Be The Hero" />
+
+          <h1>Cadastro</h1>
+          <p>
+            Faça seu cadastro, entre na plataforma e ajude as pessoas a
+            encontrarem os casos da sua ONG.
+          </p>
+
+          <Link to="/register">
+            <FiArrowLeft size="16" color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </section>
+
+        <form>
+          <input type="text" placeholder="Nome da ONG" />
+          <input type="email" placeholder="E-mail" />
+          <input type="text" placeholder="WhatsApp" />
+
+          <div className="input-group">
+            <input type="text" placeholder="Cidade" />
+            <input type="text" placeholder="UF" style={{ width: 80 }} />
+          </div>
+
+          <button type="submit" className="button">
+            Cadastrar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
