20a2122aa2fedbc24fdef7ce37d3f235983c42a5
Rubens Batista <batistarubinho@gmail.com>
2020-03-24 22:46:52 -0300

    Criado um metodo e uma rota, para fazer a listagem das ongs cadastradas no banco de dados

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -2,6 +2,12 @@ const crypto = require("crypto");
 const connection = require("../database/connection");
 
 module.exports = {
+  async index(request, response) {
+    const ongs = await connection("ongs").select("*");
+
+    return response.json(ongs);
+  },
+
   async create(request, response) {
     const { name, email, whatsapp, city, uf } = request.body;
 
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,7 @@ const OngController = require("./controllers/OngController");
 
 const routes = express.Router();
 
+routes.get("/ongs", OngController.index);
 routes.post("/ongs", OngController.create);
 
 module.exports = routes;
