e2b4c15f22477d3b9fec17087022e5a5da2f078c
Rubens Batista <batistarubinho@gmail.com>
2020-03-24 22:28:28 -0300

    Criada a rota para fazer o cadastro das ongs no banco de dados

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const crypto = require("crypto");
+const connection = require("./database/connection");
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  res.send("Hello World");
+routes.post("/ongs", async (request, response) => {
+  const { name, email, whatsapp, city, uf } = request.body;
+
+  const id = crypto.randomBytes(4).toString("HEX");
+
+  await connection("ongs").insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  });
+
+  return response.json({ id });
 });
 
 module.exports = routes;
