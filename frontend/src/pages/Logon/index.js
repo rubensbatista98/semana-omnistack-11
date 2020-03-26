286f630cef1b39fdfb1f133183c54e157c2b67cc
Rubens Batista <batistarubinho@gmail.com>
2020-03-25 23:30:55 -0300

    Criada funcionalidade responsavel por realizar a validacao de login

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { FiLogIn } from "react-icons/fi";
+
+import api from "../../services/api";
 
 import logoImage from "../../assets/logo.svg";
 import heroesImage from "../../assets/heroes.png";
@@ -8,15 +10,41 @@ import heroesImage from "../../assets/heroes.png";
 import "./styles.css";
 
 const Logon = () => {
+  const [id, setId] = useState("");
+
+  const history = useHistory();
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post("sessions", { id });
+
+      localStorage.setItem("ongId", id);
+      localStorage.setItem("ongName", response.data.name);
+
+      history.push("/profile");
+    } catch (error) {
+      alert("Falha no login, tente novamente.");
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form-section">
         <img src={logoImage} alt="Be The Hero" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input type="text" name="" id="" placeholder="Sua ID" />
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
           <button type="submit" className="button">
             Entrar
           </button>
